adf070168647520874591b23b7afe372e70d31a4
MerOmega <insaurralde30@gmail.com>
2023-03-23 14:03:19 -0300

    Simplified Recursive call

--- ejercicio_4/src/main/java/ej/ejercicio4/EarthTopography.java
+++ ejercicio_4/src/main/java/ej/ejercicio4/EarthTopography.java
@@ -2,10 +2,11 @@ package ej.ejercicio4;
 
 public class EarthTopography extends Topography{
 
-    private static final int proportion = 0;
+    private final double proportion = 0;
 
     @Override
     public double getProportion() {
-        return 0;
+        return this.proportion;
     }
+
 }
--- ejercicio_4/src/test/java/ej/ejercicio4/TopographyTest.java
+++ ejercicio_4/src/test/java/ej/ejercicio4/TopographyTest.java
@@ -1,19 +1,35 @@
 package ej.ejercicio4;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
 
 public class TopographyTest {
 
     @Test
-    void test(){
+    void testProporcion(){
         Topography waterTopo = new WaterTopography();
+        Topography waterTopo1 = new WaterTopography();
         Topography landTopo = new EarthTopography();
         Topography mixedTopo1 = new MixedTopography(Arrays.asList(waterTopo, landTopo));
         Topography mixedTopo2 = new MixedTopography(Arrays.asList(mixedTopo1, waterTopo, waterTopo));
 
+        assertEquals(0.75,mixedTopo2.getProportion());
 
-        System.out.println(mixedTopo2.getProportion()); // prints 0.5
+
+    }
+
+    @Test
+    void testEquals(){
+        Topography waterTopo = new WaterTopography();
+        Topography waterTopo1 = new WaterTopography();
+        Topography landTopo = new EarthTopography();
+        Topography mixedTopo1 = new MixedTopography(Arrays.asList(waterTopo, landTopo));
+        Topography mixedTopo2 = new MixedTopography(Arrays.asList(mixedTopo1, waterTopo, landTopo));
+        Topography mixedTopo3 = new MixedTopography(Arrays.asList(mixedTopo1, landTopo, waterTopo));
+
+        assertTrue(waterTopo.equals(waterTopo1));
+        assertFalse(mixedTopo2.equals(mixedTopo3));
     }
 }
